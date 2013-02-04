ac924bc62eebcec4abd29a662a3ade5d67293a51
Dennis Sell <dsell@dsell-tux3.(none)>
2013-02-04 02:21:48 -0600

    Reworked to function as a daemon

--- mqtt-monitor.py
+++ mqtt-monitor.py
@@ -4,7 +4,7 @@
 
 #
 # mmqtt-monitor
-#	P
+#    P
 #
 
 
@@ -12,174 +12,77 @@ __author__ = "Dennis Sell"
 __copyright__ = "Copyright (C) Dennis Sell"
 
 
+APPNAME = "mqtt-monitor"
+VERSION = "0.8"
+WATCHTOPIC = "/raw/" + APPNAME + "/command"
 
 import os
 import sys
-import mosquitto
-import socket
 import time
 import logging
-import signal
 import threading
-from config import Config
+from daemon import Daemon
+from mqttcore import MQTTClientCore
+from mqttcore import main
 
 
-CLIENT_VERSION = "0.6"
-CLIENT_NAME = "mqtt_monitor"
-MQTT_TIMEOUT = 60	#seconds
-CLIENT_BASE = "/clients/"
+class MyMQTTClientCore(MQTTClientCore):
+    def __init__(self, appname, clienttype):
+        MQTTClientCore.__init__(self, appname, clienttype)
+        self.clientversion = VERSION
+        self.watchtopic = WATCHTOPIC
+        self.monitorlist = self.cfg.MONITOR_LIST
+        self.interval = self.cfg.INTERVAL
+        self.clientversion = VERSION
+        t = threading.Thread(target=self.do_thread_loop)
+        t.start()
+        self.response = {}
+        for i in self.monitorlist:
+            self.response[i] = False 
 
 
-#TODO might want to add a lock file
-#TODO  need to deal with no config file existing!!!
-#TODO move config file to home dir
+    def on_connect(self, mself, obj, rc):
+        MQTTClientCore.on_connect(self, mself, obj, rc)
+        self.mqttc.subscribe( "/clients" + "/+/ping", 2)
 
 
-#read in configuration file
-homedir = os.path.expanduser("~")
-f = file(homedir + '/.mqtt-monitor.conf')
-cfg = Config(f)
-MQTT_HOST = cfg.MQTT_HOST
-MQTT_PORT = cfg.MQTT_PORT
-CLIENT_TOPIC = cfg.CLIENT_TOPIC
-BASE_TOPIC = cfg.BASE_TOPIC
-MONITOR_TOPIC = cfg.MONITOR_TOPIC
-MONITOR_LIST = cfg.MONITOR_LIST
-INTERVAL = cfg.INTERVAL
+    def on_message(self, mself, obj, msg):
+        MQTTClientCore.on_message(self, mself, obj, msg)
+        if (msg.topic == self.clientbase + "ping"):
+            pass  #ugly here TODO
+        else:
+            topic = msg.topic.split("/")
+            if (( topic[1] == "clients" ) and ( topic[3] == "ping") and ( msg.payload == "response" )):        
+                self.response[topic[2]] = True
+                print "reponse from ", topic[2]
+
+    def do_thread_loop(self):
+        while ( self.running ):
+            if ( self.mqtt_connected ):    
+                for client in self.monitorlist:
+                    self.response[client] = False
+                    print "Pinging ", client
+                    self.mqttc.publish( "/clients/" + client + "/ping", "request", qos=0, retain=0 )
+                time.sleep(10)
+		self.mqttc.publish("/raw/mqtt-monitor/status", "", qos=2, retain=True)
+                for client in self.monitorlist:
+                    if self.response[client] == False:
+                        print "No reponse from ", client
+                        self.mqttc.publish( "/raw/mqtt-monitor/status", "Client " + client + " is no longer responding.", qos=2, retain=True )
+                if ( self.interval ):
+                    print "Waiting ", self.interval, " minutes for next check."
+                    time.sleep(60 * self.interval)
+                else:
+                    self.running = False    #do a single shot
+            pass
 
 
-mqtt_connected = False
-response = {}
+class MyDaemon(Daemon):
+    def run(self):
+        mqttcore = MyMQTTClientCore(APPNAME, clienttype="type1")
+        mqttcore.main_loop()
 
 
-#define what happens after connection
-def on_connect(self, obj, rc):
-	global mqtt_connected
-	global running
-	global response
-
-	mqtt_connected = True
-	print "MQTT Connected"
-	mqttc.publish( CLIENT_TOPIC + "status" , "running", 1, 1 )
-	mqttc.publish( CLIENT_TOPIC + "version", CLIENT_VERSION, 1, 1 )
-	mqttc.subscribe( CLIENT_BASE + "/+/ping", 2)
-
-
-def on_message(self, obj, msg):
-	if (( msg.topic == CLIENT_BASE + "ping" ) and ( msg.payload == "request" )):
-		mqttc.publish( CLIENT_BASE + "ping", "response", qos = 1, retain = 0 )
-	else:
-		topic = msg.topic.split("/")
-		if (( topic[1] == "clients" ) and ( topic[3] == "ping") and ( msg.payload == "response" )):		
-			response[topic[2]] = True
-			print "reponse from ", topic[2]
-
-
-def do_monitor_loop():
-	global running
-	global METAR_IDS
-	global mqttc
-	global response
-	global mqtt_connected
-
-	while ( running ):
-		if ( mqtt_connected ):
-			for client in MONITOR_LIST:
-				response[client] = False
-				print "Pinging ", client
-				mqttc.publish( "/clients/" + client + "/ping", "request", 1, 0 )
-			time.sleep(10)
-			for client in MONITOR_LIST:
-				if response[client] == False:
-					print "No reponse from ", client
-					mqttc.publish( MONITOR_TOPIC, "Client " + client + " is no longer responding.", 2, 1 )
-			if ( INTERVAL ):
-				print "Waiting ", INTERVAL, " minutes for next check."
-				time.sleep(60 * INTERVAL)
-			else:
-				running = False	#do a single shot
-		pass
-
-
-def do_disconnect():
-       global mqtt_connected
-
-       mqttc.disconnect()
-       mqtt_connected = False
-       print "Disconnected"
-
-
-def mqtt_disconnect():
-	global mqtt_connected
-
-	print "Disconnecting..."
-	mqttc.disconnect()
-	if ( mqtt_connected ):
-		mqtt_connected = False 
-		print "MQTT Disconnected"
-
-
-def mqtt_connect():
-	global mqttc
-
-	rc = 1
-	while ( rc ):
-		print "Attempting connection..."
-		mqttc.will_set(CLIENT_TOPIC + "status", "disconnected_", 1, 1)
-
-		#define the mqtt callbacks
-		mqttc.on_message = on_message
-		mqttc.on_connect = on_connect
-#		mqttc.on_disconnect = on_disconnect
-
-		#connect
-		rc = mqttc.connect( MQTT_HOST, MQTT_PORT, MQTT_TIMEOUT )
-		if rc != 0:
-			logging.info( "Connection failed with error code $s, Retrying in 30 seconds.", rc )
-			print "Connection failed with error code ", rc, ", Retrying in 30 seconds." 
-			time.sleep(30)
-		else:
-			print "Connect initiated OK"
-
-
-def cleanup(signum, frame):
-	mqtt_disconnect()
-	sys.exit(signum)
-
-
-for i in MONITOR_LIST:
-	response[i] = False 
-
-#create a client
-mqttc = mosquitto.Mosquitto( CLIENT_NAME ) 
-
-#trap kill signals including control-c
-signal.signal(signal.SIGTERM, cleanup)
-signal.signal(signal.SIGINT, cleanup)
-
-running = True
-
-t = threading.Thread(target=do_monitor_loop)
-t.start()
-
-
-def main_loop():
-	global mqtt_connected
-
-	mqttc.loop(10)
-	while running:
-		if ( mqtt_connected ):
-			rc = mqttc.loop(10)
-			if rc != 0:	
-				mqtt_disconnect()
-				print rc
-				print "Stalling for 20 seconds to allow broker connection to time out."
-				time.sleep(20)
-				mqtt_connect()
-				mqttc.loop(10)
-		pass
-
-
-mqtt_connect()
-main_loop()
-
+if __name__ == "__main__":
+    daemon = MyDaemon('/tmp/' + APPNAME + '.pid')
+    main(daemon)
